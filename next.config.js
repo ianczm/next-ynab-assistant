2e9ea074469f52c1555d686277c095637c5c6d8d
ianczm <ianczm@live.co.uk>
2023-12-07 23:47:55 +0800

    Add redirect for /transactions/create.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/transactions/create",
+        destination: "/transactions/create/transportation",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
